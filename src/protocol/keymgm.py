0041edf5b0ea6749b4db36108594c5ba8d9ef597
benjaminburton <benjaminlburton@gmail.com>
2023-05-16 08:52:10 -0400

    add key management

---
+++ src/protocol/keymgm.py
@@ -0,0 +1,39 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
+
+# Generate RSA key pair
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+public_key = private_key.public_key()
+
+# Example data to sign
+data = b"Hello, blockchain!"
+
+# Sign the data
+signature = private_key.sign(
+    data,
+    padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH
+    ),
+    hashes.SHA256()
+)
+
+# Verify the signature
+try:
+    public_key.verify(
+        signature,
+        data,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    print("Signature is valid.")
+except Exception:
+    print("Signature is not valid.")
